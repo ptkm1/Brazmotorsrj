d2b54577b6c18a5d56dffbc5ac78df82aab4db69
Patrick Rocha <59058473+ptkm1@users.noreply.github.com>
2020-12-28 12:13:48 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,6 @@ export default function Index() {
       <main>
         <video id="video" width="100%" height="100%" autoPlay>
           <source src="https://video.wixstatic.com/video/ff6b3b_88fd5adc91544b28a0d0807a3c5d1060/480p/mp4/file.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
         </video>
       </main>
     </div>
