24e536daad094c0d7d100a39a34cae350066bbf3
Patrick Rocha <59058473+ptkm1@users.noreply.github.com>
2021-02-23 13:30:08 -0300

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -63,6 +63,8 @@ export default function Home({carro}) {
         <Header>
           {/* <img src={Banner} alt="Picture of the author" /> */}
           <BannerSlide />
+          <iframe src="https://teste-brazmotors.herokuapp.com/public/slide.html">
+          </iframe>
         </Header>
 
 
